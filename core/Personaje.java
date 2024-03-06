f443741c8d98ee8072074bcd3b023ebd0d354fcd
Xacobo23 <a23xacobobc@iessanclemente.net>
2024-03-06 10:09:34 +0100

    Sacado int[]

--- core/Personaje.java
+++ core/Personaje.java
@@ -30,16 +30,6 @@ public class Personaje {
     public void setNumAtaques(int numAtaques) {
         this.numAtaques = numAtaques;
     }
-
-   /*  public int[] atacar(Personaje personaje) {
-        int[] ataques = new int[numAtaques];
-        for (int i = 0; i < numAtaques; i++) {
-            ataques[i] = ataque.atacar();
-            personaje.setSalud(personaje.getSalud() - ataques[i]);
-        }
-        return ataques;
-    }*/
-
     public void setAtaque(Ataque ataque) {
         this.ataque = ataque;
     }
